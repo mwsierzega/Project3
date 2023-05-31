8c5172442fc3bd66e40dceafac1f3cdd1c686549
katmeander <kathleenmeanderson@gmail.com>
2023-05-31 10:15:02 -0700

    updates

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#from pathlib import Path
+from pathlib import Path
 
 #create the database path and enginge
 database_path = Path('optn_census.db')
@@ -16,21 +16,26 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/National_Wait_Times<br/>"
+        f"/api/v1.0/census_ethnicities_by_state<br/>"
         f"/api/v1.0/State_Wait_Times_by_Age<br/>"
         f"/api/v1.0/State_Wait_Times_by_Ethnicity<br/>"
         f"Here you will find national wait times by age and ethnicity compared to the age and ethnicity of the Census data."
     )
 
-@app.route("/api/v1.0/National_Wait_Times")
-def national_wait():
-    #return statement will have to go when query is written
-    return (f'you have reached the page')
+@app.route("/api/v1.0/<Ethnicities>")
+def census_ethn(Ethnicities):
     #create the database connection
     conn = sqlite3.connect('optn_census.db')
     #create the cursor
     c = conn.cursor()
-    #variables here, not sure yet
+    
+    data = c.execute("SELECT * FROM ethnicities_census WHERE Ethnicities = Ethnicities")
+    rows = c.fetchall()
+    data_list = []
+    for row in data:
+        data_list.append(data)
+        return jsonify(data_list)
+    #here can go a state, it will look at the national wait times
     #query code goes here, then commite
     conn.commit()
 
